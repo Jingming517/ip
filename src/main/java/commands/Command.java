b9fcada914d7baa5851dea97a77ae77334b69d21
jingming517 <liujingming517@gmail.com>
2020-09-04 18:23:53 +0800

    add dukeexception

--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -2,52 +2,53 @@ package commands;
 
 import tasks.Task;
 import java.util.ArrayList;
-
+import dukeexception.*;
 public class Command {
 
     public static void commendProcessor(ArrayList<Task> taskList, String command) {
-        String[] split = command.split(" ", 2);
-        String taskDescription;
-        if (split.length > 1) {
-            taskDescription = split[1];
-        } else {
-            taskDescription = "";
-        }
-        switch (split[0]) {
-        case "done":
-            tasks.TaskList.markTaskAsDone(taskList, split[1]);
-            break;
-        case "list":
-            messages.Message.printAllTasks(taskList);
-            break;
-        case "todo":
-            if (taskDescription != "") {
+        try {
+            String[] split = DukeExceptionDetector.extractCommandAction(command);
+            String commandType = split[0];
+            String taskDescription;
+
+            if (split.length > 1) {
+                taskDescription = split[1];
+            } else {
+                taskDescription = "";
+            }
+
+            switch (commandType) {
+            case "done":
+                tasks.TaskList.markTaskAsDone(taskList, split[1]);
+                break;
+            case "list":
+                messages.Message.printAllTasks(taskList);
+                break;
+            case "todo":
                 tasks.TaskList.addList(taskList, taskDescription, "todo");
                 messages.Message.printAddTask(taskList);
-            }
-            else {
+                break;
+            case "event":
+                tasks.TaskList.addList(taskList, taskDescription, "event");
+                messages.Message.printAddTask(taskList);
+                break;
+            case "deadline":
+                tasks.TaskList.addList(taskList, taskDescription, "deadline");
+                messages.Message.printAddTask(taskList);
+                break;
+            case "help":
+                messages.Message.printCommandInstructions();
+                break;
+            default:
                 messages.Message.printSeparationLine();
-                System.out.println("\t ☹ OOPS!!! The description of a todo cannot be empty.");
+                System.out.println("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 messages.Message.printSeparationLine();
+                break;
             }
-            break;
-        case "event":
-            tasks.TaskList.addList(taskList, taskDescription, "event");
-            messages.Message.printAddTask(taskList);
-            break;
-        case "deadline":
-            tasks.TaskList.addList(taskList, taskDescription, "deadline");
-            messages.Message.printAddTask(taskList);
-            break;
-        case "help":
-            messages.Message.printCommandInstructions();
-            break;
-        default:
-            messages.Message.printSeparationLine();
-            System.out.println("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            messages.Message.printSeparationLine();
-            break;
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
+
 
     }
 }
